3674d7ec21a7d96f50856a52b911812fc0ac49c7
Nhat K. (James) Nguyen <118421449+jimmynguyen1308@users.noreply.github.com>
2023-12-02 21:35:54 +1000

    add mongodb template

---
+++ src/dataservice/mongodb/endpoints.go
@@ -0,0 +1,27 @@
+package mongodb
+
+import "github.com/nats-io/nats.go/micro"
+
+func FindRecord() micro.HandlerFunc {
+	return func(req micro.Request) {
+
+	}
+}
+
+func InsertRecord() micro.HandlerFunc {
+	return func(req micro.Request) {
+
+	}
+}
+
+func UpdateRecord() micro.HandlerFunc {
+	return func(req micro.Request) {
+
+	}
+}
+
+func DeleteRecord() micro.HandlerFunc {
+	return func(req micro.Request) {
+
+	}
+}
